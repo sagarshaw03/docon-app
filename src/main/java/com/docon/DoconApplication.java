d638a05c0e23f30134689d26baf751917caadd3b
Sagar Shaw <sagarshaw300@gmail.com>
2022-11-23 21:07:07 +0530

    initial commit

---
+++ src/main/java/com/docon/DoconApplication.java
@@ -0,0 +1,13 @@
+package com.docon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoconApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/docon/DoconApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
